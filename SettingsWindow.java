460d3037ac78be43fe59b9fba2bb6b39e5c87a0c
Jacob Allen <enoua5@gmail.com>
2017-10-23 13:24:31 -0600

    Add diameter slider

--- SettingsWindow.java
+++ SettingsWindow.java
@@ -6,15 +6,35 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.geom.*;
 public class SettingsWindow extends JFrame
 {
+    private static final int width=300;
     public SettingsWindow()
     {
         //set title
         super("Settings");
         //make it closable
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //make a layout
+        this.setLayout(new BorderLayout());
+        //set the window size
+        this.setSize(width+100, width);
+
+        //add a panel
+        JPanel panel=new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.setBorder(BorderFactory.createLineBorder(Color.black));
+        panel.setPreferredSize(new Dimension(width, 600));
+
+        //add a scroll pane
+        final JScrollPane scroll=new JScrollPane(panel);
+        this.add(scroll, BorderLayout.CENTER);
+
+        //OPTION: diameter
+        Slider diameter=new Slider("Diameter", SettingField.DIAMETER, 10, 1000, Settings.diameter);
+        panel.add(diameter.label);
+        panel.add(diameter.slider);
+
         //put it on the screen
         this.setVisible(true);
     }
