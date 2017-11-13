d9d6fce3eb391aaff35ae6f5f2eb208823eef5bf
Jacob Allen <enoua5@gmail.com>
2017-11-13 12:11:38 -0700

    More SettingFields

--- CheckBox.java
+++ CheckBox.java
@@ -36,6 +36,7 @@ public class CheckBox implements ChangeListener
             case SEC_PRESENT:
                 Settings.sec.visible=val;
                 break;
+                
             case CARD_MARK_ROMAN:
                 Settings.cardinalMark.roman=val;
                 break;
@@ -44,6 +45,25 @@ public class CheckBox implements ChangeListener
                 break;
             case MIN_MARK_ROMAN:
                 Settings.minuteMark.roman=val;
+                break;
+                
+            case CARD_NUM_PRESENT:
+                Settings.cardinalMark.hasNumber=val;
+                break;
+            case CARD_TICK_PRESENT:
+                Settings.cardinalMark.hasTick=val;
+                break;
+            case HOUR_NUM_PRESENT:
+                Settings.hourMark.hasNumber=val;
+                break;
+            case HOUR_TICK_PRESENT:
+                Settings.hourMark.hasTick=val;
+                break;
+            case MIN_NUM_PRESENT:
+                Settings.minuteMark.hasNumber=val;
+                break;
+            case MIN_TICK_PRESENT:
+                Settings.minuteMark.hasTick=val;
                 break;
         }
     }
